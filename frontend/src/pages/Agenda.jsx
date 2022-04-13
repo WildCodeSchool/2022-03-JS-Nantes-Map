106aae3c1573df301c4866ab284712f02d58f3f1
alizeehamon <alizee.hamonpro@gmail.com>
2022-04-13 16:17:25 +0200

    fix remplissage de composants pour les routes

--- frontend/src/pages/Agenda.jsx
+++ frontend/src/pages/Agenda.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Agenda() {
-  return <div className="Agenda" />;
+  return (
+    <div className="City-tour">
+      <h1>Agenda</h1>
+    </div>
+  );
 }
 
 export default Agenda;
--- frontend/src/pages/City tour.jsx
+++ frontend/src/pages/City tour.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 function Citytour() {
-  return <div className="City-tour" />;
+  return (
+    <div className="City-tour">
+      <h1>City tour</h1>
+    </div>);
 }
 
 export default Citytour;
