aaa5ca6fc9e2ea444c6fc09470ad93ccd90c6d48
Nicolas AUVRAY <nicolas-auvray_student2022@wilder.school>
2022-04-15 09:58:20 +0200

    création du composant map en version 0.1

--- frontend/src/pages/City tour.jsx
+++ frontend/src/pages/City tour.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Map from "../components/Map";
 
 function Citytour() {
   return (
     <div className="City-tour">
       <h1>City tour</h1>
+      <Map />
     </div>
   );
 }
