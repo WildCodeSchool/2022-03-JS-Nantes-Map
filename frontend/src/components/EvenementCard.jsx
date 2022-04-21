7b5371dccf4506943474b619e0dacd8561af7d9e
alizeehamon <alizee.hamonpro@gmail.com>
2022-04-21 11:20:08 +0200

    composant Home: incoming random event & random free event

--- frontend/src/components/EvenementCard.jsx
+++ frontend/src/components/EvenementCard.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./EvenementCard.css";
 
-export default function EvenementCard() {
+export default function EvenementCard({ event }) {
   return (
     <figure className="EvenementCard">
-      <img src="evenement.image" alt="evenement.nom" />
+      <h3>{event.fields.nom}</h3>
+      <img src={event.fields.media_url} alt="evenement.nom" />
       <figcaption>
-        <h3>evenement.nom</h3>
-        <p>evenement.description</p>
-        <p>evenement.date</p>
-        <p>evenement.lieu</p>
-        <p>evenement.url</p>
+        <p className="description-event">{event.fields.description}</p>
+        <p>Date: {event.fields.date}</p>
+        <p>Lieu: {event.fields.lieu}</p>
+        <p>
+          <a href={event.fields.lien_agenda} target="_blank" rel="noreferrer">
+            En savoir plus...
+          </a>
+        </p>
       </figcaption>
     </figure>
   );
