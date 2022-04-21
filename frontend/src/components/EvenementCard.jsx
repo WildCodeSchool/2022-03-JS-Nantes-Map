6cb5059b6d87a4336d1dfe3a37dec2817b4c3921
Nicolas AUVRAY <nicolas-auvray_student2022@wilder.school>
2022-04-21 23:01:23 +0200

    finalisation du css mobile first

--- frontend/src/components/EvenementCard.jsx
+++ frontend/src/components/EvenementCard.jsx
@@ -3,19 +3,25 @@ import "./EvenementCard.css";
 
 export default function EvenementCard({ event }) {
   return (
-    <figure className="EvenementCard">
+    <div className="EvenementCard">
       <h3>{event.fields.nom}</h3>
-      <img src={event.fields.media_url} alt="evenement.nom" />
-      <figcaption>
-        <p className="description-event">{event.fields.description}</p>
-        <p>Date: {event.fields.date}</p>
-        <p>Lieu: {event.fields.lieu}</p>
-        <p>
-          <a href={event.fields.lien_agenda} target="_blank" rel="noreferrer">
-            En savoir plus...
-          </a>
-        </p>
-      </figcaption>
-    </figure>
+      <img className="photo" src={event.fields.media_url} alt="evenement.nom" />
+
+      <p className="description-event">{event.fields.description}</p>
+
+      <img className="icone" src="src/assets/date.png" alt="icone date" />
+
+      <p>{event.fields.date}</p>
+
+      <img className="icone" src="src/assets/lieu.png" alt="icone lieu" />
+
+      <p>{event.fields.lieu}</p>
+
+      <button type="button">
+        <a href={event.fields.lien_agenda} target="_blank" rel="noreferrer">
+          En savoir plus...
+        </a>
+      </button>
+    </div>
   );
 }
