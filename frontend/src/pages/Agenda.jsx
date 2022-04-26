d76a13338e49c74dd588b19b981ec1c0da55ce59
Aline Saint-Lanne <aline.saint-lanne@orange.fr>
2022-04-26 15:20:40 +0200

    ajout ordre chronologique des événements

--- frontend/src/pages/Agenda.jsx
+++ frontend/src/pages/Agenda.jsx
@@ -31,50 +31,88 @@ function Agenda() {
     )
       .then((res) => res.json())
       .then((data) => {
-        const incomingEvents = data.records.filter((event) =>
-          isComing(event.fields.date)
-        );
+        const incomingEvents = data.records
+          .filter((event) => isComing(event.fields.date))
+          .sort((event1, event2) => {
+            return new Date(event1.fields.date) - new Date(event2.fields.date);
+          });
         setEvenements(incomingEvents);
+        setFilterEvenements(incomingEvents);
       });
   }, []);
 
-  function eventSpectacle () {
-    const filterSpectacle = evenements.filter((event) => event.fields.type.includes("Théâtre") || event.fields.type.includes("Film") || event.fields.type.includes("Cirque") || event.fields.type.includes("Magie"));
+  function eventSpectacle() {
+    const filterSpectacle = evenements.filter(
+      (event) =>
+        event.fields.type.includes("Théâtre") ||
+        event.fields.type.includes("Film") ||
+        event.fields.type.includes("Cirque") ||
+        event.fields.type.includes("Magie")
+    );
     return filterSpectacle;
   }
 
-  function eventSport () {
-    const filterSport = evenements.filter((event) => event.fields.rubrique === "Sport");
+  function eventSport() {
+    const filterSport = evenements.filter(
+      (event) => event.fields.rubrique === "Sport"
+    );
     return filterSport;
   }
 
-  function eventVisit () {
-    const filterVisit = evenements.filter((event) => event.fields.type.includes("Salon") || event.fields.type.includes("Atelier") || event.fields.type.includes("Visite"));
+  function eventVisit() {
+    const filterVisit = evenements.filter(
+      (event) =>
+        event.fields.type.includes("Salon") ||
+        event.fields.type.includes("Atelier") ||
+        event.fields.type.includes("Visite")
+    );
     return filterVisit;
   }
 
-  function eventMusic () {
-    const filterMusic = evenements.filter((event) => event.fields.type.includes("Musique") || event.fields.type.includes("Chanson") || event.fields.type.includes("Danse") || event.fields.type.includes("Fanfare") || event.fields.type.includes("Poésie"));
+  function eventMusic() {
+    const filterMusic = evenements.filter(
+      (event) =>
+        event.fields.type.includes("Musique") ||
+        event.fields.type.includes("Chanson") ||
+        event.fields.type.includes("Danse") ||
+        event.fields.type.includes("Fanfare") ||
+        event.fields.type.includes("Poésie")
+    );
     return filterMusic;
   }
 
-  function eventBasketShopping () {
-    const filterBasketShopping = evenements.filter((event) => event.fields.type.includes("Brocante") || event.fields.type.includes("Marché"));
+  function eventBasketShopping() {
+    const filterBasketShopping = evenements.filter(
+      (event) =>
+        event.fields.type.includes("Brocante") ||
+        event.fields.type.includes("Marché")
+    );
     return filterBasketShopping;
   }
 
-  function eventBook () {
-    const filterBook = evenements.filter((event) => event.fields.type.includes("Conte") || event.fields.type.includes("Rencontre") || event.fields.type.includes("Conférence") || event.fields.type.includes("Activité"));
+  function eventBook() {
+    const filterBook = evenements.filter(
+      (event) =>
+        event.fields.type.includes("Conte") ||
+        event.fields.type.includes("Rencontre") ||
+        event.fields.type.includes("Conférence") ||
+        event.fields.type.includes("Activité")
+    );
     return filterBook;
   }
 
-  function eventGame () {
-    const filterGame = evenements.filter((event) => event.fields.type.includes("Jeu") || event.fields.type.includes("Fête"));
+  function eventGame() {
+    const filterGame = evenements.filter(
+      (event) =>
+        event.fields.type.includes("Jeu") || event.fields.type.includes("Fête")
+    );
     return filterGame;
   }
 
-  function eventArt () {
-    const filterArt = evenements.filter((event) => event.fields.type.includes("Expositions"));
+  function eventArt() {
+    const filterArt = evenements.filter((event) =>
+      event.fields.type.includes("Expositions")
+    );
     return filterArt;
   }
 
@@ -83,28 +121,60 @@ function Agenda() {
       <h1>AGENDA</h1>
       <h2 className="alignpictoscategories">
         <button type="button" className="pictoscategories">
-          <FontAwesomeIcon icon={faMasksTheater} className="pictoagenda" onClick={() => setFilterEvenements(eventSpectacle(evenements))}/>{" "}
+          <FontAwesomeIcon
+            icon={faMasksTheater}
+            className="pictoagenda"
+            onClick={() => setFilterEvenements(eventSpectacle(evenements))}
+          />{" "}
         </button>
         <button type="button" className="pictoscategories">
-          <FontAwesomeIcon icon={faFootball} className="pictoagenda" onClick={() => setFilterEvenements(eventSport(evenements))}/>{" "}
+          <FontAwesomeIcon
+            icon={faFootball}
+            className="pictoagenda"
+            onClick={() => setFilterEvenements(eventSport(evenements))}
+          />{" "}
         </button>
         <button type="button" className="pictoscategories">
-          <FontAwesomeIcon icon={faSignsPost} className="pictoagenda" onClick={() => setFilterEvenements(eventVisit(evenements))}/>{" "}
+          <FontAwesomeIcon
+            icon={faSignsPost}
+            className="pictoagenda"
+            onClick={() => setFilterEvenements(eventVisit(evenements))}
+          />{" "}
         </button>
         <button type="button" className="pictoscategories">
-          <FontAwesomeIcon icon={faMusic} className="pictoagenda" onClick={() => setFilterEvenements(eventMusic(evenements))}/>{" "}
+          <FontAwesomeIcon
+            icon={faMusic}
+            className="pictoagenda"
+            onClick={() => setFilterEvenements(eventMusic(evenements))}
+          />{" "}
         </button>
         <button type="button" className="pictoscategories">
-          <FontAwesomeIcon icon={faBasketShopping} className="pictoagenda" onClick={() => setFilterEvenements(eventBasketShopping(evenements))}/>{" "}
+          <FontAwesomeIcon
+            icon={faBasketShopping}
+            className="pictoagenda"
+            onClick={() => setFilterEvenements(eventBasketShopping(evenements))}
+          />{" "}
         </button>
         <button type="button" className="pictoscategories">
-          <FontAwesomeIcon icon={faBookOpen} className="pictoagenda" onClick={() => setFilterEvenements(eventBook(evenements))}/>{" "}
+          <FontAwesomeIcon
+            icon={faBookOpen}
+            className="pictoagenda"
+            onClick={() => setFilterEvenements(eventBook(evenements))}
+          />{" "}
         </button>
         <button type="button" className="pictoscategories">
-          <FontAwesomeIcon icon={faGamepad} className="pictoagenda" onClick={() => setFilterEvenements(eventGame(evenements))}/>{" "}
+          <FontAwesomeIcon
+            icon={faGamepad}
+            className="pictoagenda"
+            onClick={() => setFilterEvenements(eventGame(evenements))}
+          />{" "}
         </button>
         <button type="button" className="pictoscategories">
-          <FontAwesomeIcon icon={faPalette} className="pictoagenda" onClick={() => setFilterEvenements(eventArt(evenements))}/>{" "}
+          <FontAwesomeIcon
+            icon={faPalette}
+            className="pictoagenda"
+            onClick={() => setFilterEvenements(eventArt(evenements))}
+          />{" "}
         </button>
       </h2>
 
@@ -116,7 +186,5 @@ function Agenda() {
     </div>
   );
 }
-
-
 
 export default Agenda;
